1cd3fde34896276c3c2026ad4ef6deda29d7a1cc
Dallan Bhatti <dallan.bhatti@chatnels.com>
2020-06-27 23:50:40 -0700

    use submodules

--- services/auth/bin/__init__.py
+++
@@ -1,2 +0,0 @@
-from .init_role import init_role
-from .init_status import init_status
--- services/auth/src/resources/schemas/register.py
+++
@@ -1,7 +0,0 @@
-from marshmallow import fields, Schema
-
-
-class RegisterFormSchema(Schema):
-    email = fields.Str(required=True)
-    username = fields.Str(required=True)
-    password = fields.Str(required=True)
