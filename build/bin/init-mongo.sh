8c483e749a06c918944df45c52e76da62a1cd930
Dallan Bhatti <dallan.bhatti@techtapir.com>
2020-10-06 12:37:19 -0700

    improve init mongo

--- build/bin/init-mongo.sh
+++ build/bin/init-mongo.sh
@@ -2,6 +2,17 @@
 set -e
 
 mongo <<EOF
+use $MONGO_ADMIN_USER_DB
+db.createUser({
+    user: '$MONGO_ADMIN_USER',
+    pwd: '$MONGO_ADMIN_PWD',
+    roles: [
+        {
+            role: '$MONGO_ADMIN_USER_ROLE',
+            db: '$MONGO_ADMIN_USER_DB'
+        }
+    ]
+})
 use $MONGO_USER_DB
 db.createUser({
     user: '$MONGO_USER',
