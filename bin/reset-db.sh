23685b79ae4930e165723bc2b87b11d5a4503a59
Dallan Bhatti <dallan.bhatti@techtapir.com>
2020-11-16 13:37:38 -0800

    add the env files for new course project

--- bin/reset-db.sh
+++ bin/reset-db.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 ssh -i /home/dallanbhatti/.ssh/github super_dallan@mega <<EOF
-  # Account
-   docker exec account python manage.py reset_db \
-   && docker exec account python manage.py init
-
-  # Auth
-  docker exec auth python manage.py reset_db \
-   && docker exec auth python manage.py init
+#  # Account
+#   docker exec account python manage.py reset_db \
+#   && docker exec account python manage.py init
+#
+#  # Auth
+#  docker exec auth python manage.py reset_db \
+#   && docker exec auth python manage.py init
 
   # Contest
   docker exec contest python manage.py reset_db \
@@ -21,12 +21,12 @@ ssh -i /home/dallanbhatti/.ssh/github super_dallan@mega <<EOF
   # Score
   docker exec score_mongodb bash /bin/reset-mongo.sh
 
-  # Sport
-  docker exec sport python manage.py reset_db \
-   && docker exec sport python manage.py init
-
-  # Wager
-  docker exec wager python manage.py reset_db \
-   && docker exec wager python manage.py init
+#  # Sport
+#  docker exec sport python manage.py reset_db \
+#   && docker exec sport python manage.py init
+#
+#  # Wager
+#  docker exec wager python manage.py reset_db \
+#   && docker exec wager python manage.py init
 EOF
 exit
