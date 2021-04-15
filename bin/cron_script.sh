ebfd44bf5977ac8cdb39209dce4afb7f77c34b60
Dallan Bhatti <dallan.bhatti@techtapir.com>
2021-04-15 16:21:36 -0700

    updated the script for checking contest status on the cluster but it still needs some work on the rbac side

--- bin/cron_script.sh
+++ bin/cron_script.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-ssh -i /home/dallanbhatti/.ssh/github super_dallan@mega 'docker exec contest python manage.py check_active'
+ssh -i /home/dallanbhatti/.ssh/github dallanbhatti@rpi0 <<EOF
+export pod=$(kubectl get pod -l app=contest -o jsonpath="{.items[0].metadata.name}")
+echo $pod
+kubectl exec $pod -it -- python manage.py check_active
+EOF
 exit
