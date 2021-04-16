ad1994c40edd0961c72567d59c0dc2cf82a4a725
Dallan Bhatti <dallan.bhatti@techtapir.com>
2021-04-16 11:27:05 -0700

    just run the script on the node rn

--- bin/cron_script.sh
+++ bin/cron_script.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-ssh dallanbhatti@rpi0 <<EOF
-export pod=$(kubectl --user=super_dallan get pod -l app=contest -o jsonpath="{.items[0].metadata.name}")
-echo $pod
+pod=$(kubectl --user=super_dallan get pod -l app=contest -o jsonpath="{.items[0].metadata.name}")
 kubectl --user=super_dallan exec $pod -it -- python manage.py check_active
-EOF
-exit
+
