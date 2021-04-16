d041d6e25ae1e53d8ceb4f6cbbbe92372767b2cc
Dallan Bhatti <dallan.bhatti@techtapir.com>
2021-04-16 10:54:52 -0700

    update the rules for super_dallan

--- bin/cron_script.sh
+++ bin/cron_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-ssh -i /home/dallanbhatti/.ssh/github dallanbhatti@rpi0 <<EOF
-export pod=$(kubectl get pod -l app=contest -o jsonpath="{.items[0].metadata.name}")
+ssh dallanbhatti@rpi0 <<EOF
+export pod=$(kubectl --user=super_dallan get pod -l app=contest -o jsonpath="{.items[0].metadata.name}")
 echo $pod
-kubectl exec $pod -it -- python manage.py check_active
+kubectl --user=super_dallan exec $pod -it -- python manage.py check_active
 EOF
 exit
