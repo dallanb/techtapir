c9ef2b6e65a1847cdaccafaa9fd9f37034bd905c
Dallan Bhatti <dallan.bhatti@techtapir.com>
2021-04-20 13:56:43 -0700

    update some of the run scripts

--- bin/cron_script.sh
+++ bin/cron_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pod=$(kubectl --user=super_dallan get pod -l app=contest -o jsonpath="{.items[0].metadata.name}")
-kubectl --user=super_dallan exec $pod -it -- python manage.py check_active
+pod=$(kubectl get pod -l app=contest -o jsonpath="{.items[0].metadata.name}")
+kubectl exec $pod -it -- python manage.py check_active
 
