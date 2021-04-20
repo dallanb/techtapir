2bb4833c3485dc9abc6dce3e02a5e949354a0a08
Dallan Bhatti <dallan.bhatti@techtapir.com>
2021-04-20 11:18:49 -0700

    fix typo in new bash scripts

--- build/bin/init-secrets.sh
+++ build/bin/init-secrets.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 kubectl create secret generic env-kong-secret --from-env-file secrets/kong/.env.kong -n kong
-kubectl create secret generic s3-credentials --from-env-fiile secrets/s3/credentials.txt
-kubectl create secret generic mailer-password --from-env-fiile secrets/mailer/mailer_password.txt
+kubectl create secret generic s3-credentials --from-env-file secrets/s3/credentials.txt
+kubectl create secret generic mailer-password --from-env-file secrets/mailer/mailer_password.txt
