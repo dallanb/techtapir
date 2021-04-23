396e7b0e6ffe739d1df1eff3d38f2ca0f9f96178
Dallan Bhatti <dallan.bhatti@techtapir.com>
2021-04-23 16:03:19 -0700

    update the scripts for secrets and configmaps

--- build/bin/init-secrets.sh
+++ build/bin/init-secrets.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-kubectl create secret generic env-kong-secret --from-env-file secrets/kong/.env.kong -n kong
+kubectl create secret generic env-kong-secret --from-env-file secrets/kong/prod/.env.kong -n kong
 kubectl create secret generic secret-firebase --from-file secrets/golftapir/tech-tapir-firebase-adminsdk-3zrlh-3720b4d899.json
 kubectl create secret generic s3-credentials --from-env-file secrets/s3/credentials.txt
 kubectl create secret generic mailer-password --from-env-file secrets/mailer/mailer_password.txt
