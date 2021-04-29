2febfc8576b1b75c0e3155d4a9b4b0673351392c
Dallan Bhatti <dallan.bhatti@techtapir.com>
2021-04-29 11:36:04 -0700

    try not overlapping the namespace

--- build/bin/init-secrets.sh
+++ build/bin/init-secrets.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-kubectl create secret generic env-kong-secret --from-env-file secrets/kong/prod/.env.kong -n prod
+kubectl create secret generic env-kong-secret --from-env-file secrets/kong/prod/.env.kong -n kong-prod
 kubectl create secret generic secret-firebase --from-file secrets/golftapir/tech-tapir-firebase-adminsdk-3zrlh-3720b4d899.json -n prod
 kubectl create secret generic s3-credentials --from-env-file secrets/s3/credentials.txt -n prod
 kubectl create secret generic mailer-password --from-env-file secrets/mailer/mailer_password.txt -n prod
