2546cc4172adf677229c2dc5d7bfbdb0b5543d19
Dallan Bhatti <dallan.bhatti@techtapir.com>
2021-04-29 10:54:40 -0700

    hopefully sort of namespaces once and for all

--- build/bin/init-secrets.qaw.sh
+++ build/bin/init-secrets.qaw.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-kubectl create secret generic env-kong-secret --from-env-file secrets/kong/qaw/.env.kong -n kong-qaw
+kubectl create secret generic env-kong-secret --from-env-file secrets/kong/qaw/.env.kong -n qaw
 kubectl create secret generic secret-firebase --from-file secrets/golftapir/tech-tapir-firebase-adminsdk-3zrlh-3720b4d899.json -n qaw
 kubectl create secret generic s3-credentials --from-env-file secrets/s3/credentials.txt -n qaw
 kubectl create secret generic mailer-password --from-env-file secrets/mailer/mailer_password.txt -n qaw
